b3389dc3497f41b20f3780229a3c53847b939196
sophia <claraqqqq@gmail.com>
2015-03-29 15:11:07 -0600

    add problem description and another solution

--- ps1/ps1_3.py
+++ ps1/ps1_3.py
@@ -1,6 +1,36 @@
-# ALPHABETICAL SUBSTRINGS
-s = 'kpxiaxnrjqfpfgwo'
+# ALPHABETICAL SUBSTRINGS  (15/15 points)
 
+'''
+Assume s is a string of lower case characters.
+
+Write a program that prints the longest substring of s in which the letters 
+occur in alphabetical order. 
+
+For example, if s = 'azcbobobegghakl', then your program should print
+Longest substring in alphabetical order is: beggh
+
+In the case of ties, print the first substring. 
+
+For example, if s = 'abcbcd', then your program should print
+Longest substring in alphabetical order is: abc
+
+For problems such as these, do not include raw_input statements or define the variable s in any way. 
+Our automated testing will provide a value of s for you - 
+so the code you submit in the following box should assume s is already defined. 
+If you are confused by this instruction, please review L4 Problems 10 and 11 before 
+you begin this problem set.
+
+Note: This problem is fairly challenging. We encourage you to work smart. 
+If you've spent more than a few hours on this problem, we suggest that you 
+move on to a different part of the course. If you have time, come back to 
+this problem after you've had a break and cleared your head.
+
+'''
+
+# ALPHABETICAL SUBSTRINGS
+# s = 'kpxiaxnrjqfpfgwo'
+
+# solution 1
 cnt_longest = 0
 cnt_tmp = 1
 std = 0
@@ -23,3 +53,29 @@ for index in range(len(s)-1):
       cnt_tmp = 1
 
 print "Longest substring in alphabetical order is: ", str_longest
+
+# solution 2
+curString = s[0]
+longest = s[0]
+for i in range(1, len(s)):
+    if s[i] >= curString[-1]:
+        curString += s[i]
+        if len(curString) > len(longest):
+            longest = curString
+    else:
+        curString = s[i]
+print 'Longest substring in alphabetical order is:', longest
+
+# solution 2 function
+def alpha_substr(s):
+   str_current = s[0]
+   str_longest = s[0]
+   for idx in range(1, len(s)):
+      if s[idx] >= str_current[-1]:
+         str_current += s[idx]
+         if len(str_current) > len(str_longest):
+            str_longest = str_current
+      else:
+         str_current = s[idx]
+   print 'Longest substring in alphabetical order is:', str_longest
+
