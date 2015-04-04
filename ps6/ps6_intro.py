2ed9391237f92f25aba3cccb1ed4bd7a7aca9e60
sophia <claraqqqq@gmail.com>
2015-04-03 22:15:51 -0600

    add ps6 intro

---
+++ ps6/ps6_intro.py
@@ -0,0 +1,62 @@
+# GETTING STARTED
+
+"""
+and graded is part encryption The parts. two has set problem This
+science. computer in concept important very a encryption, with deals
+help to designed problems of set ungraded an is part recursion The
+graders provide not will We functions. recursive writing practice you
+testing and coding practice to you urge but part recursion this for
+machine. own your on problems these
+
+the includes archive zip This code_ProblemSet6.zip. save and Download
+files: following
+
+ps6_encryption.py:
+
+set. problem the part encryption the for in fill you'll code Skeleton
+
+words.txt:
+
+words English of list A
+
+ps6_pseudo.txt:
+
+until file this at look not to you urge We 2. Problem for Pseudocode
+there. contained instructions the read and 2 Problem reach you
+
+story.txt:
+
+story encoded An
+
+ps6_recursion.py:
+
+problems. recursion practice the for code Skeleton
+
+any making without environment Python a into ps6_encryption.py Load
+up set is everything that ensure to order in it, to modifications
+from words of list a loads you given have we that code The correctly.
+see should you delay, small a after okay, is everything If file. a
+out: printed following the
+
+file... from list word Loading
+loaded. words 55909
+you directory), or file such No (e.g., instead IOError an see you If
+(defined constant WORDLIST_FILENAME the of value the change should
+words. file the for pathname complete the to file) the of top the near
+file). the saved you where on based vary will (this txt
+
+implemented already functions few a has ps6_encryption.py file The
+the ignore can You solution. your up writing while use can you that
+and read should you though comments, following the between code
+else: everything understand
+
+----------------------------------- #
+code Helper #
+code) helper this understand to need don't (you #
+. . .
+code) helper of (end #
+----------------------------------- #
+
+"""
+
+
