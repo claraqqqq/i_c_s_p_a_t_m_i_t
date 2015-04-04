353b33eac7263b57c74505d184fa67dd5a6b65a7
sophia <claraqqqq@gmail.com>
2015-04-03 22:31:54 -0600

    add hail_caesar intro

---
+++ ps6/ps6_HAILCAESAR.py
@@ -0,0 +1,90 @@
+# HAIL CAESAR!
+
+"""
+
+it make to information obscuring of process the is Encryption
+people centuries, For knowledge. special without unreadable
+than better some - messages encrypt to schemes devised have
+Internet the and computer the of advent the but - others
+to not hard it's days, These field. the revolutionized
+buying are you whether encryption, of sort some encounter
+system. computer shared a into logging or online something
+trusted other with information share you lets Encryption
+disclosure. of fear without people,
+
+the (and encryption performing for algorithm an is cipher A
+called is information original The decryption). reverse,
+The ciphertext. called is it encrypted, is it After plaintext.
+the of information the all contains message ciphertext
+a by readable format a in not is it but message, plaintext
+it; decrypt to mechanism proper the without computer or human
+is it whom for those to gibberish random resemble should it
+intended. not
+
+information, auxiliary of piece a on depends usually cipher A
+encryption the into incorporated is key The key. a called
+keys different two with encrypted plaintext same the process;
+it key, the Without ciphertexts. different two have should
+into ciphertext resulting the decrypt to difficult be should
+plaintext. readable
+
+very not (though well-known a with deal will assignment This
+this In cipher. Caesar the called method encryption secure)
+and algorithms own your devise to need will you set problem
+problem. non-trivial a solve to recursion using practice will
+
+CIPHER CAESAR
+
+an pick you that is cipher Caesar the of idea basic The
+by message your of letter every shift and key, a for integer
+key your and "happy" was message your if example, For key. the
+we because on, so and "d", becomes "a" "k", becomes "h" 3, was
+is Here right. the to spots three letter every shifting are
+the to spots three shifted like looks alphabet whole the what
+right:
+
+
+z y x w v u t s r q p o n m l k j i h g f e d c b a Original:
+c b a z y x w v u t s r q p o n m l k j i h g f e d 3-shift:
+message the translate quickly can we key, above the Using
+wraps alphabet 3-shifted the how (note "kdssb" to "happy"
+c). -> z and b, -> y a, -> x so end, the at around
+
+- problem this for alphabet English the using are We Note!!
+order: following the in letters following the is, that
+
+string import >>>
+string.ascii_lowercase print >>>
+abcdefghijklmnopqrstuvwxyz
+Caesar standard the of variant a use will we problem, this In
+letters case lower and upper treat will we where cipher
+to mapped be always will letters case upper so separately,
+be always will letters case lower and letters, case upper
+"A" "c", to maps "a" if Thus, letters. case lower to mapped
+character, space the as such Characters "C". to map will
+encrypted be not will etc points, exclamation periods, commas,
+string. within characters the all basically, - cipher this by
+characters numerical all and ') (' space the plus punctuation,
+9). - (0
+
+this out check cipher, Caesar the about more learn To
+article. Wikipedia
+
+FUNCTIONS WRAPPER
+
+carefully look can you coding, start to ready are you that Now
+be not Do you. for provided have we that skeletons code the at
+we specifications function the of length the by intimidated
+on rely problems these of Many code! supplied the with provide
+that, concept coding useful extremely an functions, wrapper
+little very requires often correctly, implemented when
+that is here functions wrapper of idea The code. additional
+simple arguments as take user a to visible functions the
+other plus - arguments these supply then and inputs,
+the within only visible functions to - information
+computation. the perform to implementation
+
+more for functions wrapper on article Wikipedia the Read
+information.
+
+"""
